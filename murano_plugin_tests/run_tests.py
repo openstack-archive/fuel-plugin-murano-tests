00da8668b0ef2cfe2b08f7d5aff169fd572e872f
Oleksii Zhurba <ozhurba@mirantis.com>
2016-08-24 13:57:02 -0500

    Adding upgrade tests for Murano Fuel plugin
    Change-Id: Id8402cee1a828aa2a99c5713a5e865359b839b60

--- murano_plugin_tests/run_tests.py
+++ murano_plugin_tests/run_tests.py
@@ -41,9 +41,9 @@ class CloseSSHConnectionsPlugin(plugins.Plugin):
 def import_tests():
     from murano_plugin import test_failover  # noqa
     from murano_plugin import test_murano_plugin_bvt  # noqa
+    from murano_plugin import test_murano_plugin_update  # noqa
     from murano_plugin import test_post_install  # noqa
     from murano_plugin import test_system  # noqa
-
 
 def run_tests():
     from proboscis import TestProgram  # noqa
