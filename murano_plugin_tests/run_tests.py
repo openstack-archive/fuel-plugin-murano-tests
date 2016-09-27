b3b7c0c9eaea637043c9c48614d612812cb01eab
MStolyarenko <mstolyarenko@mirantis.com>
2016-09-27 17:22:31 +0300

    Upgrade tests
    Change-Id: I62d8769db0dc04aa5e6efe84b962047c3604a97c

--- murano_plugin_tests/run_tests.py
+++ murano_plugin_tests/run_tests.py
@@ -44,6 +44,7 @@ def import_tests():
     from murano_plugin import test_murano_plugin_update  # noqa
     from murano_plugin import test_post_install  # noqa
     from murano_plugin import test_system  # noqa
+    from murano_plugin import test_murano_maintenance_update # noqa
 
 
 def run_tests():
