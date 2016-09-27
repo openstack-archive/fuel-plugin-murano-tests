0bf886799c0b712998057fda9974dc7f61fcf19d
MStolyarenko <mstolyarenko@mirantis.com>
2016-09-27 17:22:31 +0300

    Add Upgrade test
    Check that after Maintenance Update from 9.0 to 9.1 murano plugin
    work correctly
    
    Change-Id: I62d8769db0dc04aa5e6efe84b962047c3604a97c

---
+++ murano_plugin_tests/murano_plugin/test_murano_maintenance_update.py
@@ -0,0 +1,73 @@
+#    Copyright 2016 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from fuelweb_test.helpers.decorators import log_snapshot_after_test
+from proboscis import test
+
+from murano_plugin_tests.murano_plugin import api
+
+
+@test(groups=["plugins"])
+class TestMuranoPluginMU(api.MuranoPluginApi):
+    """Class for Maintenance Update testing the Murano plugin."""
+
+    @test(depends_on_groups=["deploy_murano_plugin"],
+          groups=["test_mu_upgrade_murano_node", "murano",
+                  "test_mu_upgrade", "upgrade"])
+    @log_snapshot_after_test
+    def test_mu_upgrade_murano_node(self):
+        """Apply MU over cluster with plugin and Murano node
+
+        Scenario:
+
+        1. Deploy MOS 9.0 with Murano plugin and murano-node in non-ha
+        2. Run OSTF
+        3. Apply MU for 9.1
+        4. Run OSTF
+
+        Duration 70m
+        Snapshot test_mu_upgrade_murano_node
+
+        """
+
+        self.env.revert_snapshot("deploy_murano_plugin")
+        self.wait_plugin_online()
+        self.apply_maintenance_update_90_to_91()
+        self.run_ostf(['sanity', 'smoke'])
+        self.check_plugin_online()
+
+    @test(depends_on_groups=["deploy_murano_plugin_on_controller"],
+          groups=["test_mu_upgrade_murano_plugin", "murano",
+                  "test_mu_upgrade", "upgrade"])
+    @log_snapshot_after_test
+    def test_mu_upgrade_without_murano_node(self):
+        """Apply MU over cluster with plugin without Murano node
+
+        Scenario:
+
+        1. Deploy MOS 9.0 with Murano plugin in non-ha
+        2. Run OSTF
+        3. Apply MU for 9.1
+        4. Run OSTF
+
+        Duration 100m
+        Snapshot test_mu_upgrade_without_murano_node
+
+        """
+
+        self.env.revert_snapshot("deploy_murano_plugin_on_controller")
+        self.wait_plugin_online()
+        self.apply_maintenance_update_90_to_91()
+        self.run_ostf(['sanity', 'smoke'])
+        self.check_plugin_online()
